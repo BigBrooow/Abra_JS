13cda491b654feb00cb068f67d21fd6ea19cea64
BigBrooow <104306491+BigBrooow@users.noreply.github.com>
2024-09-13 19:57:44 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,7 @@
 const { defineConfig } = require("cypress");
 const { Client } = require('pg');
 
-const db = {user: 'heJq1mnbruN',
-    host: '172.212.108.64',
-    database: 'postgres',
-    password: 'Ul1mn7n2lMlwNuHHe',
-    port: 5432
-};
+const db = {};
 
 module.exports = defineConfig({
     e2e: {
